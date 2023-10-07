a5f24d014f40095306e3d5a903f7bf5879961ffc
MUHUMUZAJeanPierre <j.muhumuza@alustdudent.com>
2023-10-07 13:07:41 +0000

    this is alu-AirBnB_clone_v2 project

--- models/review.py
+++ models/review.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This is the review class"""
+""" Review module for the HBNB project"""
 from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey, Float
