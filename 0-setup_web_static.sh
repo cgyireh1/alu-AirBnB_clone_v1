b027a810fa014cd0250ff5072d8ae64184234187
cgyireh1 <c.gyireh@alustudent.com>
2023-10-15 13:19:32 -0700

    update

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,8 +2,13 @@
 # script that sets up web servers for the deployment of web_static
 sudo apt-get update
 sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
 
-sudo mkdir -p /data/web_static/releases/ /data/web_static/shared/test/
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
 sudo touch /data/web_static/releases/test/index.html
 sudo echo "<html>
 <head>
