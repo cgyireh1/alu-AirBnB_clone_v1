f14d56c37ac79e955057a3e2fee3b16b1360bfd9
cgyireh1 <c.gyireh@alustudent.com>
2023-10-15 13:15:03 -0700

    update

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,13 +2,8 @@
 # script that sets up web servers for the deployment of web_static
 sudo apt-get update
 sudo apt-get -y install nginx
-sudo ufw allow 'Nginx HTTP'
 
-sudo mkdir -p /data/
-sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/web_static/releases/
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/releases/ /data/web_static/shared/test/
 sudo touch /data/web_static/releases/test/index.html
 sudo echo "<html>
 <head>
@@ -20,6 +15,6 @@ sudo echo "<html>
 
 sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i '/listen 8080 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 
 sudo service nginx restart
